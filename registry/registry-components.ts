cfffd1576339f44b9a057091a366b86203982b56
Juan Pablo Solano <hello@wondercraft.co>
2024-12-09 09:44:27 -0500

    moving preset to instalable lib

--- registry/registry-components.ts
+++ registry/registry-components.ts
@@ -1,12 +1,16 @@
 import { Registry } from "./schema";
 
+const BASE_URL =
+  "https://raw.githubusercontent.com/Wondersauce/carbon-tech-showcase-templates/refs/heads/master/public/registry";
+
 export const ui: Registry = [
   {
     name: "chat-01",
     type: "registry:block",
     registryDependencies: [
-      `https://raw.githubusercontent.com/Wondersauce/carbon-tech-showcase-templates/refs/heads/master/public/registry/button-pill.json`,
-      `https://raw.githubusercontent.com/Wondersauce/carbon-tech-showcase-templates/refs/heads/master/public/registry/chat.json`,
+      `${BASE_URL}/button-pill.json`,
+      `${BASE_URL}/chat.json`,
+      `${BASE_URL}/carbon.tailwind-preset.json`,
     ],
     files: ["block/chat-01/chat-01.tsx"],
     dependencies: ["@carbon/react"],
@@ -21,6 +25,12 @@ export const ui: Registry = [
     name: "chat",
     type: "registry:ui",
     files: ["ui/chat.tsx"],
+    dependencies: ["@carbon/react"],
+  },
+  {
+    name: "carbon-tailwind-preset",
+    type: "registry:lib",
+    files: ["lib/carbon.tailwind-preset.ts"],
     dependencies: ["@carbon/react"],
   },
 ];
