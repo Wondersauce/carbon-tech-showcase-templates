710e72a3cdc7586a9f11edd9a1abed0ca6b60712
Juan Pablo Solano <hello@wondercraft.co>
2024-12-09 12:47:38 -0500

    adds icons to dependencies

--- registry/registry-components.ts
+++ registry/registry-components.ts
@@ -8,7 +8,7 @@ export const ui: Registry = [
     name: "chat-01",
     type: "registry:block",
     files: ["block/chat-01/chat-01.tsx"],
-    dependencies: ["@carbon/react"],
+    dependencies: ["@carbon/react", "@carbon/icons-react"],
     registryDependencies: [
       `${BASE_URL}/button-pill.json`,
       `${BASE_URL}/chat.json`,
@@ -19,20 +19,20 @@ export const ui: Registry = [
     name: "button-pill",
     type: "registry:ui",
     files: ["ui/button-pill.tsx"],
-    dependencies: ["@carbon/react"],
+    dependencies: ["@carbon/react", "@carbon/icons-react"],
     registryDependencies: [`${BASE_URL}/carbon-tailwind-preset.json`],
   },
   {
     name: "chat",
     type: "registry:ui",
     files: ["ui/chat.tsx"],
-    dependencies: ["@carbon/react"],
+    dependencies: ["@carbon/react", "@carbon/icons-react"],
     registryDependencies: [`${BASE_URL}/carbon-tailwind-preset.json`],
   },
   {
     name: "carbon-tailwind-preset",
     type: "registry:lib",
     files: ["lib/carbon-tailwind-preset.ts"],
-    dependencies: ["@carbon/react"],
+    dependencies: ["@carbon/react", "@carbon/icons-react"],
   },
 ];
